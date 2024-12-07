67fb9dfe73de638daae1763f5928bad052934950
Agustin Cad <agustincad123@gmail.com>
2024-12-07 14:16:37 -0300

    barra de busqueda para tazas y tazones

--- src/pages/CatalogoTazones.jsx
+++ src/pages/CatalogoTazones.jsx
@@ -1,7 +1,28 @@
-import Card from "../assets/components/Card"
-import { useNavigate } from 'react-router-dom';
+
+import React, { useState } from "react";
+import Card from "../assets/components/Card";
+import "../assets/css/styles.css";
 
 function CatalogoTazones({ agregarAlCarrito }) {
+    const [search, setSearch] = useState("");
+
+    const tazones = [
+        { id: 10, image: "../../public/images/tazon_rosa.png", name: "Taza Sonrisa Rosa", description: "Taza hecha de cerámica con temática de dibujitos", price: 132 },
+        { id: 11, image: "../../public/images/tazon_sonic.webp", name: "Taza Sonic", description: "Taza hecha de cerámica con temática de dibujitos", price: 123 },
+        { id: 12, image: "../../public/images/tazon_monster-inc.jpg", name: "Taza Monsters INC", description: "Taza hecha de cerámica con temática de Monsters INC", price: 421 },
+        { id: 13, image: "../../public/images/tazon_mike-w.jpg", name: "Taza Mike Wazowski", description: "Taza hecha de cerámica con temática de Mike Wazowski", price: 121 },
+        { id: 14, image: "../../public/images/tazon_bart.jpeg", name: "Taza Bart Simpson", description: "Taza hecha de cerámica con temática de Los Simpsons.", price: 143 },
+        { id: 15, image: "../../public/images/tazon_meeseeks.jpg", name: "Taza Meeseeks", description: "Taza hecha de cerámica con temática de Rick and Morty", price: 661 },
+        { id: 16, image: "../../public/images/tazon_kitty.jpg", name: "Taza Kitty", description: "Taza hecha de cerámica con temática de Kitty", price: 355 },
+        { id: 17, image: "../../public/images/Tazon_grogu.webp", name: "Taza Grogu", description: "Taza hecha de cerámica con temática de Mandalorian", price: 231 },
+        { id: 18, image: "../../public/images/compadre.webp", name: "Taza Calavera", description: "Taza re fachera", price: 198 }
+    ];
+
+    const filteredTazones = tazones.filter(tazon =>
+        tazon.name.toLowerCase().includes(search.toLowerCase()) ||
+        tazon.description.toLowerCase().includes(search.toLowerCase())
+    );
+
     return (
         <>
             <br />
@@ -9,76 +30,43 @@ function CatalogoTazones({ agregarAlCarrito }) {
             <br />
             <br />
 
+            <div className="container text-center">
+                <input
+                    type="text"
+                    placeholder="Buscar tazones..."
+                    value={search}
+                    onChange={(e) => setSearch(e.target.value)}
+                    className="search-bar" 
+                />
+            </div>
+
             <main className="catalogCards">
-
                 <div className="container text-center">
                     <div className="row align-items-center">
-                        <div className="col">
-                            <Card agregarAlCarrito={agregarAlCarrito} id={10} image="../../public/images/tazon_rosa.png" name="Taza Sonrisa Rosa" description="Taza hecha de cerámica con temática de dibujitos" price={132}></Card>
-                        </div>
-                        <div className="col">
-                            <Card agregarAlCarrito={agregarAlCarrito} id={11} image="../../public/images/tazon_sonic.webp" name="Taza Sonic" description="Taza hecha de cerámica con temática de dibujitos" price={123}></Card>
-                        </div>
-                        <div className="col">
-                            <Card agregarAlCarrito={agregarAlCarrito} id={12} image="../../public/images/tazon_monster-inc.jpg" name="Taza Monsters INC" description="Taza hecha de cerámica con temática de Monsters INC" price={421}></Card>
-                        </div>
+                        {filteredTazones.length > 0 ? (
+                            filteredTazones.map((tazon) => (
+                                <div className="col" key={tazon.id}>
+                                    <Card
+                                        agregarAlCarrito={agregarAlCarrito}
+                                        id={tazon.id}
+                                        image={tazon.image}
+                                        name={tazon.name}
+                                        description={tazon.description}
+                                        price={tazon.price}
+                                    />
+                                </div>
+                            ))
+                        ) : (
+                            <p>No se encontraron resultados para tu búsqueda.</p>
+                        )}
                     </div>
                 </div>
-
-                <div className="container text-center">
-                    <div className="row align-items-center">
-                        <div className="col">
-                            <Card agregarAlCarrito={agregarAlCarrito} id={13} image="../../public/images/tazon_mike-w.jpg" name="Taza Mike Wazowski" description="Taza hecha de cerámica con temática de Mike Wazowski" price={121}></Card>
-                        </div>
-                        <div className="col">
-                            <Card agregarAlCarrito={agregarAlCarrito} id={14} image="../../public/images/tazon_bart.jpeg" name="Taza Bart Simpson" description="Taza hecha de cerámica con temática de Los Simpsons." price={143}></Card>
-                        </div>
-                        <div className="col">
-                            <Card agregarAlCarrito={agregarAlCarrito} id={15} image="../../public/images/tazon_meeseeks.jpg" name="Taza Meeseeks" description="Taza hecha de cerámica con tematica de Rick and Morty" price={661}></Card>
-                        </div>
-                    </div>
-
-                </div><div className="container text-center">
-                    <div className="row align-items-center">
-                        <div className="col">
-                            <Card agregarAlCarrito={agregarAlCarrito} id={16} image="../../public/images/tazon_kitty.jpg" name="Taza Kitty" description="Taza hecha de cerámica con temática de Kitty" price={355}></Card>
-                        </div>
-                        <div className="col">
-                            <Card agregarAlCarrito={agregarAlCarrito} id={17} image="../../public/images/Tazon_grogu.webp" name="Taza Grogu" description="Taza hecha de cerámica con temática de Mandalorian" price={231}></Card>
-                        </div>
-                        <div className="col">
-                            <Card agregarAlCarrito={agregarAlCarrito} id={18} image="../../public/images/compadre.webp" name="Taza Calavera" description="Taza re fachera" price={198}></Card>
-                        </div>
-                    </div>
-                </div>
-
             </main>
             <br />
             <br />
             <br />
-
         </>
-    )
+    );
 }
 
 export default CatalogoTazones;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
